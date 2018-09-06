12e9152e166dbc521e9d1ceabe1fc0d2e2512a61
Nicola Studer <nicola.studer@braintec-group.com>
2018-09-06 10:08:35 +0200

    Create model for module

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,15 +1,24 @@
-# -*- coding: utf-8 -*-
-
 from odoo import models, fields, api
 
-# class openacademy(models.Model):
-#     _name = 'openacademy.openacademy'
+class Course(models.Model):
+    # 1. Private attributes
+    _name = 'openacademy.course'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    # 2. Default methods
+
+    # 3. Fields Declaration
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+
+    # 4. Compute and search fields, in the same order that fields declaration
+    # @api.depends('value')
+
+    # 5. Constraints and onchange methods
+
+    # 6. CRUD Methods
+
+    # 7. Action methods
+    # def _value_pc(self):
+    #     self.value2 = float(self.value) / 100
+
+    # 8. Business methods
