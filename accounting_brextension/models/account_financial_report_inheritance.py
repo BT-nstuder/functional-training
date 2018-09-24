2aac959f1555b1760b7a36770d5ce64e9dd185bb
Nicola Studer <nicola.studer@braintec-group.com>
2018-09-24 09:45:23 +0200

    Budget Report Extension: remove "get_columns_name"
    Instead of the method "get_columns_name", the columns can be added in the "apply_cmp_filter" method.

--- accounting_brextension/models/account_financial_report_inheritance.py
+++ accounting_brextension/models/account_financial_report_inheritance.py
@@ -7,41 +7,34 @@ from odoo import fields, models, api, _
 from odoo.addons.account_reports.models.account_financial_report import FormulaLine
 
 class AccountReportExtension(models.AbstractModel):
-    _inherit = "account.report"
+    _inherit = "account.financial.html.report"
 
     def apply_cmp_filter(self, options):
         res = super(AccountReportExtension, self).apply_cmp_filter(options)
-        cmp_filter = options['comparison'].get('filter')
+        cmp_filter = options['comparison'].get('filter', False)
         if cmp_filter == 'budget_comparison':
-            res['comparison']['number_period'] = 1
-            res['comparison']['periods'] = []
-            dt_from = False
-            if options['date'].get('date_from'):
-                dt_from = datetime.strptime(options['date'].get('date_from'), "%Y-%m-%d")
-            dt_to = datetime.strptime(options['date'].get('date_to'), "%Y-%m-%d")
-            if dt_from:
-                vals = {'date_from': dt_from.strftime(DEFAULT_SERVER_DATE_FORMAT),
-                        'date_to': dt_to.strftime(DEFAULT_SERVER_DATE_FORMAT),
-                        'string': 'Budgets'
-                        }
+            dt_to = options['date'].get('date_to', False)
+            if dt_to:
+                columns = ["Budget Theoretical Amount", "Budget Set Amount", "Budget Difference"]
+                res['comparison']['number_period'] = len(columns)
+                res['comparison']['periods'] = []
+                dt_from = False
+                if options['date'].get('date_from'):
+                    dt_from = datetime.strptime(options['date'].get('date_from'), "%Y-%m-%d")
+                dt_to = datetime.strptime(dt_to, "%Y-%m-%d")
+                if dt_from:
+                    for column in columns:
+                        vals = {'date_from': dt_from.strftime(DEFAULT_SERVER_DATE_FORMAT),
+                                'date_to': dt_to.strftime(DEFAULT_SERVER_DATE_FORMAT),
+                                'string': column
+                                }
+                        res['comparison']['periods'].append(vals)
+                else:
+                    for column in columns:
+                        vals = {'date': dt_to.strftime(DEFAULT_SERVER_DATE_FORMAT), 'string': column}
+                        res['comparison']['periods'].append(vals)
+                res['comparison']['string'] = 'Budget Comparison'
             else:
-                vals = {'date': dt_to.strftime(DEFAULT_SERVER_DATE_FORMAT), 'string': 'Budgets'}
-            res['comparison']['periods'].append(vals)
-            res['comparison']['string'] = 'Budget Comparison'
-            return res
+                res['comparison']['number_period'] = 0
+                res['comparison']['periods'] = []
         return res
-
-class FinancialReportExtension(models.Model):
-    _inherit = "account.financial.html.report"
-
-    def get_columns_name(self, options):
-        res = super(FinancialReportExtension, self).get_columns_name(options)
-        if options.get('comparison') and options['comparison'].get('periods'):
-            if options['comparison'].get('filter') == 'budget_comparison':
-                print("Mein Filter wurde gesetzt.")
-                res = res[:len(res)-2]
-                res += [{'name': 'Budget Theoretical Amount', 'class': 'number'},
-                        {'name': 'Budget Set Amount', 'class': 'number'},
-                        {'name': 'Budget Difference', 'class': 'number'}]
-        return res
-
